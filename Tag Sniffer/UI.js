6d1bbc399f8f230772b4b06f27999d0a4cc183e4
DigiMarketingPA <digitalmarketing@performanceautomotive.com>
2026-09-15 14:57:50 -0400

    Delete Tag Sniffer directory

--- Tag Sniffer/UI.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('clearButton').addEventListener('click', () => {
-    document.getElementById('results').innerHTML = ''; // Clear results
-    document.getElementById('urlInput').value = ''; // Clear input field
-}); 
